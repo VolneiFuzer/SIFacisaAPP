54708af470502637fcd4e004c2bf9ebc986fe7ac
Bueno Nepomuceno <buenonp@gmail.com>
2016-12-06 21:39:05 -0300

    Implementado noticias com firebase

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,10 +1,33 @@
 angular.module('app.controllers', [])
   
-.controller('notCiasCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+.controller('notCiasCtrl', ['$scope', '$stateParams', '$location',// The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
 // You can include any angular dependencies as parameters for this function
 // TIP: Access Route Parameters for your page via $stateParams.parameterName
-function ($scope, $stateParams) {
+function ($scope, $stateParams, $location) {
 
+	firebase.database().ref('noticias').on('value', function(snapshot) {
+		$scope.noticias = snapshot.val();
+		$scope.$apply();
+	});
+
+	$scope.goToNoticia = function(id) {
+		$location.path('side-menu21/noticia').search({id: id});
+	}
+
+}])
+
+.controller('noticiaCtrl', ['$scope', '$stateParams', '$location', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams, $location) {
+
+	var id = $location.search().id;
+
+	firebase.database().ref('noticias/' + id).once('value').then(function(snapshot) {
+		$scope.noticia = snapshot.val();
+		$scope.$apply();
+	});
+	
 
 }])
    
@@ -29,8 +52,9 @@ function ($scope, $stateParams) {
 // TIP: Access Route Parameters for your page via $stateParams.parameterName
 function ($scope, $stateParams) {
 
-	firebase.database().ref('/sobre/').once('value').then(function(snapshot) {
+	firebase.database().ref('sobre').once('value').then(function(snapshot) {
 		$scope.sobre = snapshot.val();
+		$scope.$apply();
 	});
 
 
@@ -41,8 +65,9 @@ function ($scope, $stateParams) {
 // TIP: Access Route Parameters for your page via $stateParams.parameterName
 function ($scope, $stateParams) {
 
-	firebase.database().ref('/periodos/').once('value').then(function(snapshot) {
+	firebase.database().ref('periodos').on('value', function(snapshot) {
 		$scope.periodos = snapshot.val();
+		$scope.$apply();
 	});
 
 }])
@@ -52,8 +77,9 @@ function ($scope, $stateParams) {
 // TIP: Access Route Parameters for your page via $stateParams.parameterName
 function ($scope, $stateParams) {
 
-	firebase.database().ref('/professores/').once('value').then(function(snapshot) {
+	firebase.database().ref('professores').on('value', function(snapshot) {
 		$scope.professores = snapshot.val();
+		$scope.$apply();
 	});
 
 }])
